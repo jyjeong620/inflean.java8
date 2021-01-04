ffa1b4723832a48a8d069dec43aeb13cafd22a2f
jyjeong620 <jyjeong620@naver.com>
2021-01-04 15:58:30 +0900

    refactor : Java8 새로추가된 기본 메소드 예제

--- src/main/java/inflean/java8/InterfaceEx/App.java
+++ src/main/java/inflean/java8/InterfaceEx/App.java
@@ -1,9 +1,29 @@
 package inflean.java8.InterfaceEx;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Spliterator;
+
 public class App {
     public static void main(String[] args) {
         Foo foo = new DefaultFoo("jyjeong");
         foo.printName();
         foo.printNameUpperCase();
+
+        List<String> name = new ArrayList<>();
+
+        name.add("jyjeong");
+        name.add("test");
+        name.add("foo");
+
+        name.forEach(System.out::println); //for 문 대용
+
+        Spliterator<String> spliterator = name.spliterator();
+        Spliterator<String> stringSpliterator = spliterator.trySplit();
+        while (spliterator.tryAdvance(System.out::println));
+        System.out.println("==================");
+        while (stringSpliterator.tryAdvance(System.out::println));
+
+
     }
 }
